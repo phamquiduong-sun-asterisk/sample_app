49214b1f294ebf5c77b70e84719ae5792e4ba97f
Phạm Quí Dương <85145251+phamquiduong-sun-asterisk@users.noreply.github.com>
2021-06-15 14:53:55 +0700

    Chapter_7 Sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
+
     get "static_pages/home"
+    get "home", to: "static_pages#home"
     get "static_pages/help"
+    get "help", to: "static_pages#help"
+
+    get "signup", to: "users#new"
+    post "signup", to: "users#create"
+    
+    resources :users, only: %i(new create show)
   end
 end
